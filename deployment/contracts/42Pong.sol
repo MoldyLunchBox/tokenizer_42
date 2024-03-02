0f6f7e69d15949648632bfffef82767e4b05518b
Abderrahmane Mya <duu116@gmail.com>
2024-03-02 06:02:46 +0100

    [+] created basic pong42 token

--- deployment/contracts/42Pong.sol
+++ deployment/contracts/42Pong.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.8.20;
+
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Pong42 {
+contract Pong42 is ERC20{
+    constructor() ERC20("pong42", "p42"){
+        _mint(msg.sender, 10000);
+    }
 
 }
