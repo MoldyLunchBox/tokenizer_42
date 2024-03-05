8aae5eecc0225ee65ce394b2e02de2a4c34fcc79
Abderrahmane Mya <duu116@gmail.com>
2024-03-05 04:40:08 +0100

    [+] added more methods to teh contract
    transfer method
    allowence

--- deployment/contracts/Pong42.sol
+++ deployment/contracts/Pong42.sol
@@ -1,41 +1,44 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Pong42 is ERC20Capped, Ownable {
-    constructor() ERC20("pong42", "p42") ERC20Capped(1000) Ownable() {
+    constructor(uint256 cap) ERC20("Pong42", "P42") ERC20Capped(cap) {}
+
+    function mintTokens(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
     }
 
-    function minting(address addr, uint256 amount) external onlyOwner {
-        _mint(addr, amount);
+    function transferTokens(address to, uint256 amount) external returns (bool) {
+        _transfer(_msgSender(), to, amount);
+        return true;
     }
 
-    /**
-     * @dev Returns the balance of tokens held by owner
-     * @return The balance of tokens held by owner
-     */
-    function getMyBalance() external view returns (uint256) {
-        return balanceOf(msg.sender);
+    function transferTokensFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool) {
+        _transfer(from, to, amount);
+        _approve(
+            from,
+            _msgSender(),
+            allowance(from, _msgSender()) - amount
+        );
+        return true;
     }
 
-
-   /**
-     * @dev Returns the balance of tokens held by the specified address.
-     * @param account The address for which to check the balance.
-     * @return The balance of tokens held by the specified address.
-     */
-    function getBalance(address account) external view returns (uint256) {
+    function getTokenBalance(address account) external view returns (uint256) {
         return balanceOf(account);
     }
 
-  /**
-     * @dev Function to mint a certain amount of tokens.
-     * @param recipient The address that will receive the minted tokens.
-     * @param amount The amount of tokens to mint.
-     */
-    function mintTokens(address recipient, uint256 amount) external onlyOwner {
-        _mint(recipient, amount);
+    function getTokenAllowance(address owner, address spender)
+        external
+        view
+        returns (uint256)
+    {
+        return allowance(owner, spender);
     }
-
 }
