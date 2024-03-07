c643eca1f91c681cd8a4526f442bd47352476920
Abderrahmane Mya <duu116@gmail.com>
2024-03-08 00:36:58 +0100

    [+] working contract

--- deployment/contracts/Pong42.sol
+++ deployment/contracts/Pong42.sol
@@ -1,41 +1,48 @@
-pragma solidity ^0.8.0;
+// contracts/MyaToken.sol
+// SPDX-License-Identifier: MIT
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+pragma solidity ^0.8.17;
 
-contract Pong42 is ERC20Capped, Ownable {
-    constructor() ERC20("pong42", "p42") ERC20Capped(1000) Ownable() {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract Pong42 is ERC20Capped, ERC20Burnable {
+    address payable public owner;
+    uint256 public blockReward;
+
+    constructor(uint256 cap, uint256 reward) ERC20("pong42", "p42") ERC20Capped(cap * (10 ** decimals())) {
+        owner = payable(msg.sender);
+        _mint(owner, 70000000 * (10 ** decimals()));
+        blockReward = reward * (10 ** decimals());
     }
 
-    function minting(address addr, uint256 amount) external onlyOwner {
-        _mint(addr, amount);
+    function _mint(address account, uint256 amount) internal virtual override(ERC20Capped, ERC20) {
+        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+        super._mint(account, amount);
     }
 
-    /**
-     * @dev Returns the balance of tokens held by owner
-     * @return The balance of tokens held by owner
-     */
-    function getMyBalance() external view returns (uint256) {
-        return balanceOf(msg.sender);
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, blockReward);
     }
 
-
-   /**
-     * @dev Returns the balance of tokens held by the specified address.
-     * @param account The address for which to check the balance.
-     * @return The balance of tokens held by the specified address.
-     */
-    function getBalance(address account) external view returns (uint256) {
-        return balanceOf(account);
+    function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
+        if(from != address(0) && to != block.coinbase && block.coinbase != address(0) && ERC20.totalSupply() + blockReward <= cap()) {
+            _mintMinerReward();
+        }
+        super._beforeTokenTransfer(from, to, value);
     }
 
-  /**
-     * @dev Function to mint a certain amount of tokens.
-     * @param recipient The address that will receive the minted tokens.
-     * @param amount The amount of tokens to mint.
-     */
-    function mintTokens(address recipient, uint256 amount) external onlyOwner {
-        _mint(recipient, amount);
+    function setBlockReward(uint256 reward) public onlyOwner {
+        blockReward = reward * (10 ** decimals());
     }
 
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
 }
