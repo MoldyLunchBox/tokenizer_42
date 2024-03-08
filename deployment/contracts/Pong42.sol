85380fdf6e9fe86f99ef5d5b1ad7e3c7e5996691
Abderrahmane Mya <duu116@gmail.com>
2024-03-08 02:26:27 +0100

    [+] deployed the new code

--- deployment/contracts/Pong42.sol
+++ deployment/contracts/Pong42.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract Pong42 is ERC20Capped, ERC20Burnable {
+contract Pong42 is ERC20Capped {
     address payable public owner;
 
     constructor(uint256 cap) ERC20("pong42", "P42") ERC20Capped(cap * (10 ** decimals())) {
@@ -15,7 +15,7 @@ contract Pong42 is ERC20Capped, ERC20Burnable {
         _mint(owner, 70000000 * (10 ** decimals()));
     }
 
-    function _mint(address account, uint256 amount) internal virtual override(ERC20Capped, ERC20) {
+    function _mint(address account, uint256 amount) internal virtual override(ERC20Capped) {
         require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
         super._mint(account, amount);
     }
